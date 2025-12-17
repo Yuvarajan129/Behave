1688baf7ca7555cfdd11742add4d5cc63f7ac85d
Yuvaraj <yuvarajans129@gmail.com>
2025-12-17 12:16:14 +0530

    old one

---
+++ features/login.feature
@@ -0,0 +1,20 @@
+# Feature: Login feature
+#   Scenario: Successful login
+#     Given the user is on the login page
+#     When the user enters valid credentials
+#     Then they should be logged in
+# Scenario: Unsuccessful login
+#     Given the user is on the login page
+#     When the user enters invalid credentials
+#     Then they should see an error message
+
+# Scenario: Invalid login test
+#   Given the user is on the login page
+#   When the user enters invalid credentials
+#   And the user clicks the login button
+#   Then they should see an error message
+#   And they should remain on the login page
+
+# # Scenario: Check sum
+# #   Given I add 2 and 2
+# #   Then result should be 5
---
+++ features/steps/login_steps.py
@@ -0,0 +1,50 @@
+# from behave import *
+
+# @given("the user is on the login page")
+# def step_given_login_page(context):
+#     print("Opening login page...")
+
+# @when("the user enters valid credentials")
+# def step_when_credentials(context):
+#     print("Entering username and password...")
+
+# @then("they should be logged in")
+# def step_then_logged_in(context):
+#     print("User is logged in!")
+
+# @then("they should see an error message")
+# def step_then_error_message(context):
+#     print("Displaying error message for invalid credentials.")
+
+# @when("the user enters invalid credentials")
+# def step_when_invalid_credentials(context):
+#     print("Entering invalid username and password...")
+
+# @when("the user clicks the login button")
+# def step_when_click_login(context):
+#     print("Clicking the login button...")
+
+# @then("they should be redirected to the dashboard")
+# def step_then_redirect_dashboard(context):
+#     print("Redirecting to the dashboard...")
+
+# @then("they should remain on the login page")
+# def step_then_remain_login_page(context):
+#     print("Staying on the login page...")
+
+# @when("the user forgets to enter the password")
+# def step_when_forget_password(context):
+#     print("Password field is left empty...")
+
+# @then("they should see a password required message")
+# def step_then_password_required(context):
+#     print("Displaying password required message.")
+
+
+# # @given("I break the test")
+# # def step_impl(context):
+# #     assert False, "Boom! Test failed on purpose."
+
+# # @then("result should be {value}")
+# # def step_impl(context, value):
+# #     assert context.result == int(value)
